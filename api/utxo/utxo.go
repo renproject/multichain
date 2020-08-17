ed64dc09a534d0bf86e18ac5e795655f17f44ecb
Loong <bzlw@pm.me>
2020-08-17 11:42:22 +1000

    document apis

--- api/utxo/utxo.go
+++ api/utxo/utxo.go
@@ -1,3 +1,6 @@
+// Package utxo defines the UTXO API. All chains that use a utxo-based model
+// should implement this API. The UTXO API is used to send and confirm
+// transactions between addresses.
 package utxo
 
 import (
@@ -7,63 +10,84 @@ import (
 	"github.com/renproject/pack"
 )
 
+// An Outpoint identifies a specific output produced by a transaction.
 type Outpoint struct {
 	Hash  pack.Bytes `json:"hash"`
 	Index pack.U32   `json:"index"`
 }
 
+// An Output is produced by a transaction. It includes the conditions required
+// to spend the output (called the pubkey script, based on Bitcoin).
 type Output struct {
 	Outpoint
 	Value        pack.U256  `json:"value"`
 	PubKeyScript pack.Bytes `json:"pubKeyScript"`
 }
 
+// An Input specifies an existing output, produced by a previous transaction, to
+// be consumed by another transaction. It includes the script that meets the
+// conditions specified by the consumed output (called the sig script, based on
+// Bitcoin).
 type Input struct {
 	Output
 	SigScript pack.Bytes `json:"sigScript"`
 }
 
+// A Recipient specifies an address, and an amount, for which a transaction will
+// produce an output. Depending on the output, the address can take on different
+// formats (e.g. in Bitcoin, addresses can be P2PK, P2PKH, or P2SH).
 type Recipient struct {
 	To    address.Address `json:"to"`
 	Value pack.U256       `json:"value"`
 }
 
+// The Tx interfaces defines the functionality that must be exposed by
+// utxo-based transactions.
 type Tx interface {
 	// Hash returns the hash that uniquely identifies the transaction.
 	// Generally, hashes are irreversible hash functions that consume the
 	// content of the transaction.
 	Hash() (pack.Bytes, error)
 
-	// Inputs returns the transaction inputs consumed by the transaction.
+	// Inputs consumed by the transaction.
 	Inputs() ([]Input, error)
 
-	// Outputs returns the transaction outputs produced by the transaction.
+	// Outputs produced by the transaction.
 	Outputs() ([]Output, error)
 
-	// Sighashes returns the digests that must be signed before the transaction
-	// can be submitted by the client.
+	// Sighashes that must be signed before the transaction can be submitted by
+	// the client.
 	Sighashes() ([]pack.Bytes32, error)
 
 	// Sign the transaction by injecting signatures for the required sighashes.
-	// The serialized public key used to sign the sighashes must also be
-	// specified.
+	// The serialized public key used to sign the sighashes should also be
+	// specified whenever it is available.
 	Sign([]pack.Bytes65, pack.Bytes) error
 
-	// Serialize the transaction into bytes. Generally, this is the format in
-	// which the transaction will be submitted by the client.
+	// Serialize the transaction into bytes. This is the format in which the
+	// transaction will be submitted by the client.
 	Serialize() (pack.Bytes, error)
 }
 
+// The TxBuilder interface defines the functionality required to build
+// account-based transactions. Most chain implementations require additional
+// information, and this should be accepted during the construction of the
+// chain-specific transaction builder.
 type TxBuilder interface {
 	BuildTx([]Input, []Recipient) (Tx, error)
 }
 
+// The Client interface defines the functionality required to interact with a
+// chain over RPC.
 type Client interface {
-	// Output returns the transaction output uniquely identified by the given
-	// transaction outpoint. It also returns the number of confirmations for the
-	// transaction output.
+	// Output returns the transaction output identified by the given outpoint.
+	// It also returns the number of confirmations for the output. If the output
+	// cannot be found before the context is done, or the output is invalid,
+	// then an error should be returned.
 	Output(context.Context, Outpoint) (Output, pack.U64, error)
 
-	// SubmitTx to the underlying blockchain network.
+	// SubmitTx to the underlying chain. If the transaction cannot be found
+	// before the context is done, or the transaction is invalid, then an error
+	// should be returned.
 	SubmitTx(context.Context, Tx) error
 }
