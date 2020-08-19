e42df5b9800e4b305cc977882f05b6367b05fd52
Rohit Narurkar <rohit.narurkar@protonmail.com>
2020-08-19 15:13:23 +0530

    testing address impl for filecoin

--- chain/filecoin/address.go
+++ chain/filecoin/address.go
@@ -5,10 +5,19 @@ import (
 	"github.com/renproject/multichain/api/address"
 )
 
-type AddressEncoder struct{}
+type AddressEncoderDecoder struct {
+	AddressEncoder
+	AddressDecoder
+}
 
-func NewAddressEncoder() AddressEncoder {
-	return AddressEncoder{}
+type AddressEncoder struct{}
+type AddressDecoder struct{}
+
+func NewAddressEncoderDecoder() AddressEncoderDecoder {
+	return AddressEncoderDecoder{
+		AddressEncoder: AddressEncoder{},
+		AddressDecoder: AddressDecoder{},
+	}
 }
 
 func (encoder AddressEncoder) EncodeAddress(raw address.RawAddress) (address.Address, error) {
@@ -17,12 +26,6 @@ func (encoder AddressEncoder) EncodeAddress(raw address.RawAddress) (address.Add
 		return address.Address(""), err
 	}
 	return address.Address(addr.String()), nil
-}
-
-type AddressDecoder struct{}
-
-func NewAddressDecoder() AddressDecoder {
-	return AddressDecoder{}
 }
 
 func (addrDecoder AddressDecoder) DecodeAddress(addr address.Address) (address.RawAddress, error) {
