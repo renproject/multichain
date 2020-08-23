ad83e20e2d493285ccda6c4b02200c230a9b8b9b
Jacob Gadikian <jacobgadikian@gmail.com>
2020-08-23 20:50:47 +0700

    used correct peercoin library

--- chain/peercoin/peercoin_test.go
+++ chain/peercoin/peercoin_test.go
@@ -2,7 +2,6 @@ package peercoin_test
 
 import (
 	"context"
-	"github.com/renproject/multichain/chain/bitcoin"
 	"log"
 	"os"
 	"reflect"
@@ -87,7 +86,7 @@ var _ = Describe("Peercoin", func() {
 						Value: pack.NewU256FromU64(pack.NewU64((output.Value.Int().Uint64() - 1000) / 3)),
 					},
 				}
-				tx, err := bitcoin.NewTxBuilder(&chaincfg.RegressionNetParams).BuildTx(inputs, recipients)
+				tx, err := peercoin.NewTxBuilder(&chaincfg.RegressionNetParams).BuildTx(inputs, recipients)
 				Expect(err).ToNot(HaveOccurred())
 
 				// Get the digests that need signing from the transaction, and
