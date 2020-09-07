7c0f3c2436f0b4229f515732aca0be56517ec198
Janet Liang <56005637+janet-harmony@users.noreply.github.com>
2020-09-07 13:33:30 -0700

    [API] Address & Gas API implementations (#2)
    * [api] Address API EncodeDecoder implementation
    * [api] Gas API Implementation

--- chain/harmony/address.go
+++ chain/harmony/address.go
@@ -1,1 +1,46 @@
 package harmony
+
+import (
+	"github.com/btcsuite/btcutil/bech32"
+	"github.com/renproject/multichain/api/address"
+)
+
+type EncoderDecoder struct {
+	address.Encoder
+	address.Decoder
+}
+
+func NewEncoderDecoder() address.EncodeDecoder {
+	return EncoderDecoder{
+		Encoder: NewEncoder(),
+		Decoder: NewDecoder(),
+	}
+}
+
+type Encoder struct {}
+
+func (Encoder) EncodeAddress(addr address.RawAddress) (address.Address, error) {
+	encodedAddr, err := bech32.ConvertBits(addr, 8, 5, true)
+	if err != nil {
+		return "", err
+	}
+	return address.Address(encodedAddr), nil
+}
+
+type Decoder struct {}
+
+func (Decoder) DecodeAddress(addr address.Address) (address.RawAddress, error) {
+	_, decodedAddr, err := bech32.Decode(string(addr))
+	if err != nil {
+		return nil, err
+	}
+	return decodedAddr[:], nil
+}
+
+func NewEncoder() address.Encoder {
+	return Encoder{}
+}
+
+func NewDecoder() address.Decoder {
+	return Decoder{}
+}
