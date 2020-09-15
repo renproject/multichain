e9eff1d49345d0a34e8734ea5a83e1696c02a05e
Rohit Narurkar <rohit.narurkar@protonmail.com>
2020-09-16 03:54:20 +0530

    add gas API for Filecoin

---
+++ chain/filecoin/gas.go
@@ -0,0 +1,30 @@
+package filecoin
+
+import (
+	"context"
+
+	"github.com/renproject/multichain/api/gas"
+	"github.com/renproject/pack"
+)
+
+// A GasEstimator returns the gas-per-byte that is needed in order to confirm
+// transactions with an estimated maximum delay of one block. In distributed
+// networks that collectively build, sign, and submit transactions, it is
+// important that all nodes in the network have reached consensus on the
+// gas-per-byte.
+type GasEstimator struct {
+	gasPerByte pack.U256
+}
+
+// NewGasEstimator returns a simple gas estimator that always returns the same
+// amount of gas-per-byte.
+func NewGasEstimator(gasPerByte pack.U256) gas.Estimator {
+	return &GasEstimator{
+		gasPerByte: gasPerByte,
+	}
+}
+
+// EstimateGasPrice returns gas required per byte for Cosmos-compatible chains.
+func (gasEstimator *GasEstimator) EstimateGasPrice(ctx context.Context) (pack.U256, error) {
+	return gasEstimator.gasPerByte, nil
+}
