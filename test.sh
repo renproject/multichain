bfdb3846c2c1d26cd7ff155baae9382c31a1e6a1
Johannes Borg <johannes.borg@hotmail.com>
2020-11-22 23:35:20 +0100

    Move Qtum testing to execute first for debugging convenience

--- test.sh
+++ test.sh
@@ -5,8 +5,9 @@ COMPOSE_PARALLEL_LIMIT=10 #DEZU: Added because of error, apparently some library
 docker-compose -f ./infra/docker-compose.yaml up --build -d
 echo "Waiting for multichain to boot..."
 sleep 30
-echo "Done waiting, running tests..." # DEZU: Added for debugging
-$GO version # DEZU: Added for de lulz
+echo "Done waiting, running tests omn Qtum..." # DEZU: Added for debugging
+$GO test -v ./chain/qtum/...
+echo "Done with Qtum, running all tests..." # DEZU: Added for debugging
 $GO test -v ./...
 echo "Done testing, closing..." # DEZU: Added for debugging
 docker-compose -f ./infra/docker-compose.yaml down
