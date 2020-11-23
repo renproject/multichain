4e4c1bae321b92ab81b08fad6f9af0a1d85837fd
Johannes Borg <johannes.borg@hotmail.com>
2020-11-23 21:26:12 +0100

    Clean up the docker-side script a bit

--- infra/qtum/run.sh
+++ infra/qtum/run.sh
@@ -20,14 +20,14 @@ echo "(QTUM): Trying to generate 501 blocks..." # DEZU: TODO: Remove debug
 /app/bin/qtum-cli generatetoaddress 501 $ADDRESS
 echo "(QTUM): Blocks (hopefully) generated!" # DEZU: TODO: Remove debug
 
-echo "(QTUM): Running 'getbalance'..." # DEZU: TODO: Remove debug
-/app/bin/qtum-cli getbalance
+#echo "(QTUM): Running 'getbalance'..." # DEZU: TODO: Remove debug
+#/app/bin/qtum-cli getbalance
 
-echo "(QTUM): Running 'getwalletinfo'..." # DEZU: TODO: Remove debug
-/app/bin/qtum-cli getwalletinfo
+#echo "(QTUM): Running 'getwalletinfo'..." # DEZU: TODO: Remove debug
+#/app/bin/qtum-cli getwalletinfo
 
-echo "(QTUM): Running 'estimatesmartfee 10'..." # DEZU: TODO: Remove debug
-/app/bin/qtum-cli estimatesmartfee 10
+#echo "(QTUM): Running 'estimatesmartfee 10'..." # DEZU: TODO: Remove debug
+#/app/bin/qtum-cli estimatesmartfee 10
 
 #echo "(QTUM): Running 'listunspent 0 999999999 qb15NCu3w4zyd14L21P99AdqmovHCiCEqC'..." # DEZU: TODO: Remove debug
 #/app/bin/qtum-cli listunspent 0 999999999 qb15NCu3w4zyd14L21P99AdqmovHCiCEqC | cat
@@ -37,11 +37,11 @@ while :
 do
     echo "(QTUM): Running 'generatetoaddress 1'..." # DEZU: TODO: Remove debug
     /app/bin/qtum-cli generatetoaddress 1 $ADDRESS
-    sleep 1
+    sleep 5
     # send tx to own address while paying fee to the miner
     echo "(QTUM): Running 'sendtoaddress $ADDRESS 0.5 "" "" true'..." # DEZU: TODO: Remove debug
     /app/bin/qtum-cli sendtoaddress $ADDRESS 0.5 "" "" true
-    sleep 1
-    echo "(QTUM): Running 'estimatesmartfee 15'..." # DEZU: TODO: Remove debug
-    /app/bin/qtum-cli estimatesmartfee 10
+    sleep 5
+    #echo "(QTUM): Running 'estimatesmartfee 15'..." # DEZU: TODO: Remove debug
+    #/app/bin/qtum-cli estimatesmartfee 10
 done
