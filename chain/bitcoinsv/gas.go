0b49a51a5f397dc7062c85f8d42f45dfa20367db
onlyonbitcoin <80013025+onlyonbitcoin@users.noreply.github.com>
2021-03-03 21:44:02 -0500

    Add BSV files

---
+++ chain/bitcoinsv/gas.go
@@ -0,0 +1,9 @@
+package bitcoinsv
+
+import "github.com/renproject/multichain/chain/bitcoin"
+
+// GasEstimator re-exports bitcoin.GasEstimator.
+type GasEstimator = bitcoin.GasEstimator
+
+// NewGasEstimator re-exports bitcoin.NewGasEstimator.
+var NewGasEstimator = bitcoin.NewGasEstimator
