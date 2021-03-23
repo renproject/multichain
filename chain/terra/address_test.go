7c085240ff9912520ff9217af53596bdc249ce7d
Rohit Narurkar <rohit.narurkar@protonmail.com>
2021-03-23 11:32:24 +0530

    test: add more invalid address scenarios

--- chain/terra/address_test.go
+++ chain/terra/address_test.go
@@ -13,16 +13,32 @@ import (
 
 var _ = Describe("Terra", func() {
 	Context("when decoding address", func() {
-		Context("when decoding Terra address", func() {
+		decoder := terra.NewAddressDecoder()
+		Context("when decoding a valid address", func() {
 			It("should work", func() {
-				decoder := terra.NewAddressDecoder()
-
 				addrStr := "terra1ztez03dp94y2x55fkhmrvj37ck204geq33msma"
 				_, err := decoder.DecodeAddress(multichain.Address(pack.NewString(addrStr)))
 				Expect(err).ToNot(HaveOccurred())
-
-				addrStr = ""
-				_, err = decoder.DecodeAddress(multichain.Address(pack.NewString(addrStr)))
+			})
+		})
+		Context("when decoding an address with invalid prefix", func() {
+			It("should fail", func() {
+				addrStr := "cosmosztez03dp94y2x55fkhmrvj37ck204geq33msma"
+				_, err := decoder.DecodeAddress(multichain.Address(pack.NewString(addrStr)))
+				Expect(err).To(HaveOccurred())
+			})
+		})
+		Context("when decoding an invalid address", func() {
+			It("should fail", func() {
+				addrStr := "terra1ztez03dp94y2x55fkhmrvj37ck204geq33msm"
+				_, err := decoder.DecodeAddress(multichain.Address(pack.NewString(addrStr)))
+				Expect(err).To(HaveOccurred())
+			})
+		})
+		Context("when decoding an empty address", func() {
+			It("should fail", func() {
+				addrStr := ""
+				_, err := decoder.DecodeAddress(multichain.Address(pack.NewString(addrStr)))
 				Expect(err).Should(MatchError(fmt.Errorf("unexpected address length: want=20, got=0")))
 			})
 		})
