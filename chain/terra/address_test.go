a1a0b5bf97a871c41b7c4044d68198101ecd9425
Rohit Narurkar <rohit.narurkar@protonmail.com>
2021-03-23 11:15:09 +0530

    fix: cosmos addrs are expected to be 20-bytes long

--- chain/terra/address_test.go
+++ chain/terra/address_test.go
@@ -1,6 +1,8 @@
 package terra_test
 
 import (
+	"fmt"
+
 	"github.com/renproject/multichain"
 	"github.com/renproject/multichain/chain/terra"
 	"github.com/renproject/pack"
@@ -18,6 +20,10 @@ var _ = Describe("Terra", func() {
 				addrStr := "terra1ztez03dp94y2x55fkhmrvj37ck204geq33msma"
 				_, err := decoder.DecodeAddress(multichain.Address(pack.NewString(addrStr)))
 				Expect(err).ToNot(HaveOccurred())
+
+				addrStr = ""
+				_, err = decoder.DecodeAddress(multichain.Address(pack.NewString(addrStr)))
+				Expect(err).Should(MatchError(fmt.Errorf("unexpected address length: want=20, got=0")))
 			})
 		})
 	})
