a52ebf85d163f2e4693202d0920422125ebd1250
Amos .E <anjeas4@gmail.com>
2021-06-09 13:35:51 +0100

    Add Decred DEPRIV_KEY/ADDRESS Run Daemons in screen/ sinmet minning

--- infra/decred/run.sh
+++ infra/decred/run.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+ADDRESS=SsaGEEZu2L8x93qvKzzahtzQ7yzkec3i8wL
+PRIV_KEY=PsUQEpYDXVwphd9xNXUMj63LyxSWPTor3RDgfw9DMdH9tDkJaosyp
 
 # Start
-/decred/dcrd --simnet
+screen -dm /decred/dcrd --simnet --miningaddr=$ADDRESS
+screen -dm /decred/dcrwallet --simnet --createtemp
 sleep 2
 
 # Print setup
-echo "DCRD STARTED"
+echo "DECRED_ADDRESS=$ADDRESS"
+
+/decred/dcrctl --simnet generate 101
+
+# Simulate mining
+while :
+do
+	/decred/dcrctl --simnet generate 10
+    sleep 5
+done
+
