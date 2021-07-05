adcb7f16c56ed0f611cc29c6e55755c677782579
Rohit Narurkar <rohit.narurkar@protonmail.com>
2021-07-05 16:42:01 +0400

    fix: fix the blocktime and chain id for bsc+eth

--- infra/binance/run.sh
+++ infra/binance/run.sh
@@ -2,13 +2,14 @@
 MNEMONIC=$1
 ADDRESS=$2
 
-ganache-cli                     \
-  -h 0.0.0.0                    \
-  -a 105                        \
-  -k muirGlacier                \
-  -i 420                        \
-  -m "$MNEMONIC"                \
-  -p 8575                       \
-  -u $ADDRESS                   \
-  -b 10                         \
-  --chainId 1337
+ganache-cli      \
+  -h 0.0.0.0     \
+  -a 105         \
+  -k muirGlacier \
+  -i 420         \
+  -m "$MNEMONIC" \
+  -p 8575        \
+  -u $ADDRESS    \
+  -b 1           \
+  -l 60000000    \
+  --chainId 420
