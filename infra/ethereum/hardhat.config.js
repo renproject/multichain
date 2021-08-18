c1e8645e6768dc7758f2bb310744c202ed8843ca
Soumya Ghosh Dastidar <gdsoumya@gmail.com>
2021-08-18 10:30:55 +0530

    updated ethereum test chain to hardhat
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>

---
+++ infra/ethereum/hardhat.config.js
@@ -0,0 +1,22 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.7.3",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      blockGasLimit: 15000000,
+      hardfork: "london",
+      mining: {
+        auto: false,
+        interval: 1000
+      },
+      accounts: {
+        mnemonic: "clutch captain shoe salt awake harvest setup primary inmate ugly among become",
+        count: 105
+      }
+    }
+  }
+}
