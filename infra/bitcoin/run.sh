58461fa05ca12eb06e6b6a5c63b09f62bc98727b
Rahul Ghangas <rahulghangas0@gmail.com>
2021-08-20 04:18:52 +0530

    fix: update bitcoin client for race condition fixed in later release

--- infra/bitcoin/run.sh
+++ infra/bitcoin/run.sh
@@ -3,28 +3,31 @@ ADDRESS=$1
 PRIV_KEY=$2
 
 # Start
-/app/bin/bitcoind
-sleep 10
+/app/bin/bitcoind -regtest -daemon
+sleep 20
 
 # Print setup
 echo "BITCOIN_ADDRESS=$ADDRESS"
 
+# Create wallet
+/app/bin/bitcoin-cli createwallet "testwallet"
+
 # Import the address
-/app/bin/bitcoin-cli importaddress $ADDRESS
+/app/bin/bitcoin-cli -regtest importaddress $ADDRESS
 
 # Import the private key to spend UTXOs
-/app/bin/bitcoin-cli importprivkey $PRIV_KEY
+/app/bin/bitcoin-cli -regtest importprivkey $PRIV_KEY
 
 # Generate enough block to pass the maturation time
-/app/bin/bitcoin-cli generatetoaddress 101 $ADDRESS
+/app/bin/bitcoin-cli -regtest generatetoaddress 101 $ADDRESS
 
 # Simulate mining
 while :
 do
     # generate new btc to the address
-    /app/bin/bitcoin-cli generatetoaddress 1 $ADDRESS
+    /app/bin/bitcoin-cli -regtest generatetoaddress 1 $ADDRESS
     sleep 5
     # send tx to own address while paying fee to the miner
-    /app/bin/bitcoin-cli sendtoaddress $ADDRESS 0.5 "" "" true
+    /app/bin/bitcoin-cli -regtest sendtoaddress $ADDRESS 1 "" "" true
     sleep 5
 done
