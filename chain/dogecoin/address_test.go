37711e1bfc945420945d42774ad9be4b876cb996
tok-kkk <yunshi@renproject.io>
2022-05-31 10:48:21 +1000

    add comment for the test

--- chain/dogecoin/address_test.go
+++ chain/dogecoin/address_test.go
@@ -13,6 +13,7 @@ var _ = Describe("Dogecoin", func() {
 	Context("when decoding segwit address", func() {
 		Context("when decoding an address from a different network ", func() {
 			It("should return an error ", func() {
+				// A valid bitcoin segwit address which is not a valid doge address
 				addr := multichain.Address("bc1qk6yk2ctcu2pmtxfzhya692h774562vlv2g7dvl")
 				decoder := bitcoin.NewAddressDecoder(&dogecoin.MainNetParams)
 				_, err := decoder.DecodeAddress(addr)
